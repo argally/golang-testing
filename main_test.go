b4278f498c6edc43f37c0c85263f98dde6fd90a2
argally <amandargalligan@gmail.com>
2025-03-01 20:03:33 +0000

    refactor: use table driven tests to reduce duplication

--- main_test.go
+++ main_test.go
@@ -2,14 +2,37 @@ package main
 
 import "testing"
 
+// Test_isPrime tests the isPrime function to determine if a number is prime.
+// It iterates over a set of predefined test cases, each containing a name,
+// a test number, the expected result, and a message. The function checks
+// if the result from isPrime matches the expected result and reports an
+// error if there is a mismatch.
 func Test_isPrime(t *testing.T) {
-	result, msg := isPrime(0)
-	if result {
-		t.Errorf("with %d as test parameter got true expected false", 0)
-
+	primeTests := []struct {
+		name     string
+		testNum  int
+		expected bool
+		msg      string
+	}{
+		{"prime", 7, true, "7 is a prime number"},
+		{"not prime", 8, false, "8 is not a prime number as it is divisible by 2"},
+		{"negative", -1, false, "-1 is a negative number and not a prime"},
+		{"zero", 0, false, "0 is not a prime number"},
 	}
-	if msg != "0 is not a prime number" {
-		t.Errorf("wrong message returned: %s", msg)
-	}
 
+	for _, entry := range primeTests {
+
+		result, msg := isPrime(entry.testNum)
+		if entry.expected && !result {
+			t.Errorf("Test case %s failed: expected true, got false", entry.name)
+		}
+
+		if !entry.expected && result {
+			t.Errorf("Test case %s failed: expected false, got true", entry.name)
+		}
+
+		if entry.msg != msg {
+			t.Errorf("Test case %s failed: expected %s, got %s", entry.name, entry.msg, msg)
+		}
+	}
 }
